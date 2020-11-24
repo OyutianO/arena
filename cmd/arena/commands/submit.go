06535216cb4a8bdb58ea9f79129a54be052018bc
OyutianO <510693455@qq.com>
2020-11-24 09:53:15 +0800

    修改linux命令以便获取当前登录用户名

--- cmd/arena/commands/submit.go
+++ cmd/arena/commands/submit.go
@@ -141,7 +141,7 @@ func (s *submitArgs) transform() (err error) {
 		}
 	}
 
-	scratch := "/scratch/" + util.Get_linux_command("echo $HOME")
+	scratch := "/scratch/" + util.Get_linux_command("echo $USER")
 	s.DataDirs = append(s.DataDirs, dataDirVolume{
 		Name:          fmt.Sprintf("scratch"),
 		HostPath:      scratch,
